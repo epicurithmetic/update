5bad637c778bd0f7d81cb650cae19b33613efbd9
Rob <rhsculling@gmail.com>
2020-04-04 01:01:41 +1300

    Quanta headlines update + some planning

--- updates.py
+++ updates.py
@@ -10,14 +10,12 @@ from bs4 import BeautifulSoup
 seconds = time.time()
 local_time = time.ctime(seconds)
 
-
 # Clear the terminal
 os.system("cls")
 
 # Some simple graphics to frame the update.
 x = "|||"
 y = "-"
-
 
 print(x + y*169 + x)
 print(x + y*169 + x)
@@ -28,16 +26,42 @@ print(" ")
 
 # Greeting
 print("Greetings Robert. \n")
-print("Your updates are as follows: \n")
+print("Your requested updates are as follows: \n")
 
 
 # RNZ headlines
-print("Radio New Zealand Headlines: ")
+#print("Today's Radio New Zealand headlines: ")
 
 # Quanta stories
+print(" "*5 + "Today's Quanta Magazine headlines: \n  ")
+def QuantaHeadlines():
 
+    url_quanta = "https://www.quantamagazine.org/"
+    response_quanta = requests.get(url_quanta)
+    soup_quanta = BeautifulSoup(response_quanta.text, "html.parser")
+
+    headlines = []
+
+    # This gets the primary headline on the homepage.
+    main = soup_quanta.find("h1", class_="noe mv0 h0 color-transition hover--orange")
+    headlines.append(main.text)
+
+    # This gets the rest of the stories on the homepage.
+    sub_headlines = soup_quanta.find_all("h2", class_="card__title noe mv0 theme__accent-hover transition--color")
+    for x in sub_headlines:
+        headlines.append(x.text)
+
+    return headlines
+
+quanta_stories = QuantaHeadlines()
+i = 1
+for story in quanta_stories:
+    print(" "*10 + str(i) + ". " + story)
+    i += 1
+
+print("\n")
 # Cryptocurrency prices
-print("Cryptocurrency summary: \n ")
+print(" "*5 + "Here is the summary of the cryptocurriencies you're interested in: \n")
 
 def BitCoin_price():
 
@@ -71,12 +95,23 @@ def Ethereum_price():
 print(" "*10 + "BitCoin  is currently worth " + BitCoin_price() + " (USD)")
 print(" "*10 + "Ethereum is currently worth " + Ethereum_price() + " (USD)")
 
-
+print("\n")
 # NZX prices
+print(" "*5 + "These are the prices of the NZX stocks that you want to keep an eye on: \n")
 
+print("\n")
 # Weather forecast
 
-# Dinner idea: recent PuL recipe. 
+print("\n")
+# Music updates bandcamp pages
+# Ultimae, Synphaera, Carbon Based Lifeforms, White Label Records, 1631 Recordings
+
+print("\n")
+# Merriam-Webster word of the day
+#print("Today's word of the day is: " + MW_WotD)
+
+print("\n")
+# Dinner idea: recent PuL recipe.
 
 
 print(" ")
